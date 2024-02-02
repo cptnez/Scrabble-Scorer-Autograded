a7a0b227a0571584107d52f771cd319c141dbf34
Grant Tracey <gtracey24@gmail.com>
2024-02-02 13:35:11 -0600

    Almost finished with task 2

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -31,28 +31,103 @@ function oldScrabbleScorer(word) {
 
 // your job is to finish writing these functions and variables that we've named //
 // don't change the names or your program won't work as expected. //
-
+let answer;
 function initialPrompt() {
-   console.log("Let's play some scrabble! Enter a word:");
+   let response = input.question("Let's play some scrabble! Enter a word: ");
+   answer = simpleScorer(response);
+   return console.log(answer);
 };
 
-let simpleScorer;
+let simpleLetters = {
+   1: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+}
 
-let vowelBonusScorer;
+function simpleScorer(word) {
+	word = word.toUpperCase();
+	let letterPoints = "";
+ 
+	for (let i = 0; i < word.length; i++) {
+ 
+	  for (const pointValue in simpleLetters) {
+ 
+		 if (simpleLetters[pointValue].includes(word[i])) {
+			letterPoints += `Points for '${word[i]}': ${pointValue}\n`
+		 }
+ 
+	  }
+	}
+	return letterPoints;
+ };
+
+ let vowelLetters = {
+   1: ['B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z'],
+   3: ['A', 'E', 'I', 'O', 'U']
+};
+
+function vowelBonusScorer(word) {
+	word = word.toUpperCase();
+	let letterPoints = "";
+ 
+	for (let i = 0; i < word.length; i++) {
+ 
+	  for (const pointValue in vowelLetters) {
+ 
+		 if (vowelLetters[pointValue].includes(word[i])) {
+			letterPoints += `Points for '${word[i]}': ${pointValue}\n`
+		 }
+ 
+	  }
+	}
+	return letterPoints;
+ };
+
 
 let scrabbleScorer;
 
-const scoringAlgorithms = [];
+let simpleScorerObject = {
+   name: '0) Simple Scorer',
+   description: 'Each letter is worth 1 point.',
+   scoringFunction: 'simpleScorer'
+  }
+  
+let vowelBonusScorerObject = {
+  name: '1) Vowel Bonus Scorer',
+  description: 'Vowels are 3 pts, consonants are 1 pt.',
+  scoringFunction: 'vowelBonusScorer'
+}
 
-function scorerPrompt() {}
+let oldScrabbleScorerObject = {
+  name: '2) Old Scrabble Scorer',
+  description: 'The traditional scoring algorithm.',
+  scoringFunction: 'oldScrabbleScorer'
+}
+const scoringAlgorithms = [simpleScorerObject, vowelBonusScorerObject, oldScrabbleScorerObject];
+
+function scorerPrompt(arr) {
+let word = input.question("Let's play some scrabble! Enter a word: ");
+   console.log('Which scoring system do you want to use?');
+   for (let i = 0; i < 3; i++){
+      console.log(`-${arr[i].name}: ${arr[i].description}`)
+   }
+   let scoreChoice = input.question('Enter 0, 1, or 2: ');
+   if (scoreChoice == 0) {
+    return console.log(simpleScorer(word));
+   } else if (scoreChoice == 1) {
+    return console.log(vowelBonusScorer(word));
+   } else if (scoreChoice == 2) {
+    return console.log(oldScrabbleScorer(word));
+   } else {
+    return input.question('Enter 0, 1, or 2: ');
+   }
+};
 
 function transform() {};
 
 let newPointStructure;
 
 function runProgram() {
-   initialPrompt();
-   
+  //  initialPrompt();
+   scorerPrompt(scoringAlgorithms);
 }
 
 // Don't write any code below this line //
